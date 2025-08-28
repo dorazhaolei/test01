8d31c67962304783e8379bfb588950f32c8f8174
zhaolei <zhaolei@dora.design>
2025-08-28 17:26:47 +0800

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,7 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
